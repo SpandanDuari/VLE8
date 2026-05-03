34433731df25f1c646fc6514dc69138c26cf36b5
Spandan Duari <137663824+SpandanDuari@users.noreply.github.com>
2026-05-03 14:11:22 +0530

    Update project name to 'VLE 8 - Blue Green'

--- app/app.js
+++ app/app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 // Project info
 const STUDENT = "Spandan Duari";
-const PROJECT = "VLE 8 - Blue Green Deployment";
+const PROJECT = "VLE 8 - Blue Green";
 
 // Root route (UI)
 app.get("/", (req, res) => {
